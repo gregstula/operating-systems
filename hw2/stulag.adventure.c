afb77c1e55779a6239408abce0fd84daf025a417
Greg Stula <gregstula@me.com>
2020-02-08 01:21:55 -0500

    Time crunch

--- hw2/stulag.adventure.c
+++ hw2/stulag.adventure.c
@@ -37,7 +37,7 @@ typedef enum room_type {
 */
 
 
-char* get_newest_dirname()
+char* get_newest_dirname(void)
 {
     int newest_dir_time = -1;
     char target_prefix[32] = "stulag.rooms.";
@@ -68,35 +68,79 @@ char* get_newest_dirname()
     return newest_dir_name;
 }
 
+
+void process_room(char* room_name) {
+    /* open the start room file */
+    FILE* fptr = NULL;
+    /* array of lines */
+    char** lines = calloc(20, sizeof(char*));
+    char line_buffer[ENOUGH_SPACE];
+    int line_count = 0;
+
+    fptr = fopen(room_name, "r+");
+    if (fptr) {
+    /* Get the last line to know the room type */
+        while (fgets(line_buffer, sizeof(line_buffer), fptr) != NULL) {
+            /* allocate for line_buff + nul char */
+            lines[line_count] = malloc(sizeof(char) * ENOUGH_SPACE);
+            strcpy(lines[line_count], line_buffer);
+            line_count++;
+        }
+    }
+    int i;
+    /* Get connections */
+    /* will be in between first and last lines */
+    for (i =1 ; i < line_count - 1; i++) {
+        printf("%s\n",lines[i]);
+    }
+
+}
+
 int main(void)
 {
     char* dir_name = get_newest_dirname();
 
     DIR* dptr = opendir(dir_name);
     struct dirent* file_in_dir;
-    FILE* fptr;
+    FILE* fptr = NULL;
 
+    /* buffers */
     char file_name[ENOUGH_SPACE];
+    char line_buffer[ENOUGH_SPACE];
+    char start_file[ENOUGH_SPACE];
 
 
+    /* First find the start room */
     if (dptr != NULL) {
         while((file_in_dir = readdir(dptr)) != NULL) {
-            char line_buffer[ENOUGH_SPACE];
             if(file_in_dir->d_type != DT_DIR) { /* looking for non-directories */
+                /* sprintf to concat directory name with the file name
+                 * this is for fopen later */
                 sprintf(file_name, "%s/%s", dir_name, file_in_dir->d_name);
-                printf("%s\n", file_name);
+
+                /* open file, remember to close at end of scope */
                 fptr = fopen(file_name, "r+");
+
                 if (fptr) {
                     /* Get the last line to know the room type */
                     while (fgets(line_buffer, sizeof(line_buffer), fptr) != NULL) continue;
                 }
-                printf("%s\n", line_buffer);
-                break;
+                /* compare last line to see if it is the start room */
+                if(strcmp("ROOM TYPE: START_ROOM\n", line_buffer) == 0) {
+                    /* save the start file name */
+                    sprintf(start_file, "%s", file_name);
+                }
+                /* close file at end of scopt */
+                fclose(fptr);
             }
         }
     }
 
-    /* free dyn alloc strind */
+    process_room(start_file);
+
+   /* close what we oppened */
+    closedir(dptr);
+    /* free dyn alloc string */
     free(dir_name);
     return 0;
 }
