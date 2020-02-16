3a66651889f28727583d8017ceae415a75331637
Greg Stula <gregstula@me.com>
2020-02-16 18:31:32 -0500

    Finished

--- hw3/mypython.py
+++ hw3/mypython.py
@@ -1,4 +1,8 @@
 #!/bin/env python3
+
+# Gregory D Stula
+# 2020-02-16
+# CS344 Winter 2020
 import random
 
 # open three files
@@ -13,3 +17,26 @@ for i in range(0,3):
     # close the file
     f.close()
 
+# print the contents of each file
+for i in range(0,3):
+    #open each one
+    f=open(f"file{i}","r+")
+    # read the 10 random chars and print them
+    text=f.read(10)
+    print(text)
+
+
+# genrate 2 random nums to print
+num1=random.randint(1,42)
+num2=random.randint(1,42)
+
+
+#print them
+print(num1)
+print(num2)
+
+#get the product
+prod = num1 * num2
+
+#print it
+print(prod)
