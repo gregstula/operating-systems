176a0998d31eea90f7a765a8c1ef52f0fc45b72f
Greg Stula <gregstula@me.com>
2020-02-16 17:06:49 -0500

    Open three files and write random chars

--- hw3/mypython.py
+++ hw3/mypython.py
@@ -1,6 +1,13 @@
 #!/bin/env python3
+import random
 
-f=open("fil1","w+")
-
+for i in range(0,3):
+    # open three files
+    f=open(f"file{i}","w+")
+    for i in range(0,11):
+        # write random lowecase ascii letter (97-122)
+        x = chr(random.randint(97,122))
+        f.write(x)
 # close the file
-f.close()
+    f.close()
+
