7571794755e6b0eda496ca6420d45402215b5d9a
Greg Stula <gregstula@me.com>
2020-02-16 17:07:51 -0500

    add newline

--- hw3/mypython.py
+++ hw3/mypython.py
@@ -1,13 +1,15 @@
 #!/bin/env python3
 import random
 
+# open three files
 for i in range(0,3):
-    # open three files
     f=open(f"file{i}","w+")
-    for i in range(0,11):
+    for i in range(0,10):
         # write random lowecase ascii letter (97-122)
         x = chr(random.randint(97,122))
         f.write(x)
-# close the file
+    # last letter must be newline
+    f.write('\n')
+    # close the file
     f.close()
 
